6b7d64ab2d70ef71c04b93a6694521af81e9af3a
Emanuel Evans <emanuel.evans@gmail.com>
2012-06-16 13:08:22 -0600

    Automatically start SLIME for Clojure files

--- personal/personal-clojure.el
+++ personal/personal-clojure.el
@@ -5,5 +5,10 @@
   (interactive)
   (browse-url "http://clojure.github.com"))
 
-
 (define-key clojure-mode-map "\C-c\C-dr" 'open-clojure-docs)
+
+(defun set-up-clojure-mode ()
+  (clojure-jack-in))
+
+(add-hook 'clojure-mode-hook
+          'set-up-clojure-mode)
