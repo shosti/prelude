06fda9737b0916736c92f4b8cb700eefa6e60566
Emanuel Evans <emanuel.evans@gmail.com>
2012-07-07 11:51:35 -0600

    Fix clojure configuration
    Turns out automatically jacking in is a bad idea

--- personal/personal-clojure.el
+++ personal/personal-clojure.el
@@ -6,9 +6,3 @@
   (browse-url "http://clojure.github.com"))
 
 (define-key clojure-mode-map "\C-c\C-dr" 'open-clojure-docs)
-
-(defun set-up-clojure-mode ()
-  (clojure-jack-in))
-
-(add-hook 'clojure-mode-hook
-          'set-up-clojure-mode)
