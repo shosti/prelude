a3fa409f8f1b3ad18593fd4fd01040731c3a00c4
Emanuel Evans <emanuel.evans@gmail.com>
2012-08-20 15:33:38 -0700

    Tweak css mode; reformatting

--- personal/personal-packages.el
+++ personal/personal-packages.el
@@ -1,19 +1,44 @@
 (require 'personal-functions)
 
 (defvar personal-prelude-modules
-  '(prelude-c prelude-clojure prelude-common-lisp prelude-emacs-lisp
-              prelude-haskell prelude-js prelude-latex prelude-lisp
-              prelude-programming prelude-ruby prelude-scheme
-              prelude-xml prelude-coffee))
+  '(prelude-c
+    prelude-clojure
+    prelude-coffee
+    prelude-common-lisp
+    prelude-emacs-lisp
+    prelude-haskell
+    prelude-js
+    prelude-latex
+    prelude-lisp
+    prelude-programming
+    prelude-ruby
+    prelude-scheme
+    prelude-xml))
 
 (defvar personal-packages
   (append personal-prelude-modules
-          '(ac-slime ace-jump-mode ascope auto-complete bitlbee
-                     buffer-move elscreen gnuplot google-c-style
-                     highlight-parentheses js-comint parenface
-                     pretty-mode smex undo-tree c-eldoc
-                     elisp-slime-nav coffee-mode python mark-multiple
-                     yaml-mode jinja2-mode))
+          '(ac-slime
+            ace-jump-mode
+            ascope
+            auto-complete
+            bitlbee
+            buffer-move
+            c-eldoc
+            coffee-mode
+            elisp-slime-nav
+            elscreen
+            gnuplot
+            google-c-style
+            highlight-parentheses
+            jinja2-mode
+            js-comint
+            mark-multiple
+            parenface
+            pretty-mode
+            python
+            smex
+            undo-tree
+            yaml-mode))
   "A list of packages to ensure are installed at launch.")
 
 (defun personal-packages-installed-p ()
--- personal/personal-scss.el
+++
@@ -1,5 +0,0 @@
-(defun set-up-scss-mode ()
-  (set-up-programming)
-  (set-up-auto-indent))
-
-(add-hook 'scss-mode-hook 'set-up-scss-mode)
