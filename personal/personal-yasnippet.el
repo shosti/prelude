09a9a0546e902ddbd42b0da64628fe492c141e38
Emanuel Evans <emanuel.evans@gmail.com>
2012-08-25 11:15:55 -0700

    Fix yasnippet bug with tab key

--- personal/personal-yasnippet.el
+++ personal/personal-yasnippet.el
@@ -3,6 +3,6 @@
 (require 'yasnippet)
 
 (setq yas/snippet-dirs (list (concat prelude-personal-dir "snippets")))
-(yas/reload-all)
 ;; Play nice with auto-complete
 (setq yas/trigger-key "\C-m")
+(yas/reload-all)
