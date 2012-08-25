e8805a19754dde811368b8f135a247f87d60fcae
Emanuel Evans <emanuel.evans@gmail.com>
2012-08-25 10:47:04 -0700

    Configure mu4e

---
+++ personal/personal-mu4e.el
@@ -0,0 +1,19 @@
+(require 'personal-packages)
+
+(add-to-list 'load-path
+             "/usr/local/share/emacs/site-lisp/mu4e")
+(require 'mu4e)
+
+(setq mu4e-sent-folder "/Sent Messages"
+      mu4e-drafts-folder "/[Gmail].Drafts"
+      mu4e-trash-folder "/[Gmail].Trash"
+      mu4e-get-mail-command "offlineimap"
+      mu4e-sent-messages-behavior 'delete
+      mu4e-maildir-shortcuts '(("/[Gmail].Important"   . ?i)
+                               ("/[Gmail].Sent Mail"   . ?s)
+                               ("/[Gmail].Trash"       . ?t)
+                               ("/[Gmail].All Mail"    . ?a))
+      user-mail-address "emanuel.evans@gmail.com"
+      user-full-name "Emanuel Evans"
+      message-send-mail-function 'message-send-mail-with-sendmail
+      message-kill-buffer-on-exit t)
